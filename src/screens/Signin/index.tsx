327c1f242f95e4bae2f066bc234e4033809245d2
CELS0 <celsojr.300@gmail.com>
2021-11-06 21:12:46 -0300

    feat

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -1,11 +1,39 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StatusBar,
+} from "react-native";
 import { styles } from "./styles";
+import IllustrationImg from '../../assets/illustration.png'
+import { Buttonicon } from "../../components/Buttonicon";
 
-export  function Signin() {
+export function Signin() {
   return (
     <View style={styles.container}>
-      <Text>Hello World, nlw Together</Text>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      <Image
+        source={IllustrationImg}
+        style={styles.image}
+        resizeMode="stretch"
+      />
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize {`\n`}
+          suas jogatinas{`\n`}
+          facilmente
+        </Text>
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games{`\n`}
+          favoritos com seus amigos
+        </Text>
+      <Buttonicon title="Entrar com Discord" activeOpacity={0.7} />
+      </View>
     </View>
   );
 }
