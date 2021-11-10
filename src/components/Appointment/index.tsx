431748489d983275bc37b7328f6ca231f657a1c8
CELS0 <celsojr.300@gmail.com>
2021-11-09 21:02:11 -0300

    feat

--- src/components/Appointment/index.tsx
+++ src/components/Appointment/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { View,Text } from 'react-native';
+import { View, Text } from 'react-native';
 import { styles } from './styles';
 import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
 import { GuildIcon } from '../GuildIcon';
 import { categories } from '../../utils/categories';
 
+import PlayerSvg from '../../assets/player.svg';
+import CalendarSvg from '../../assets/calendar.svg'
+
+import { theme } from '../../global/styles/theme';
 
 type GuildProps = {
     id: string,
@@ -27,7 +31,10 @@ type Props = RectButtonProps & {
 
 
 export function Appointment({ data, ...rest }: Props) {
-const [category] = categories.filter(item => item.id === data.category)
+    const [category] = categories.filter(item => item.id === data.category);
+    const { owner } = data.guild;
+    const { primary, on } = theme.colors;
+
     return (
         <RectButton {...rest} >
             <View style={styles.container}>
@@ -35,10 +42,34 @@ const [category] = categories.filter(item => item.id === data.category)
                 <View style={styles.content}>
                     <View style={styles.header}>
                         <Text style={styles.title}>{data.guild.name}</Text>
+
+                        <Text style={styles.category}>
+                            {category.title}
+                        </Text>
+                    </View>
+
+                    <View style={styles.footer}>
+                        <View style={styles.dateInfo}>
+                            <CalendarSvg />
+
+                            <Text style={styles.date}>
+                                {data.date}
+                            </Text>
+                        </View>
+
+
+                        <View style={styles.playerInfo}>
+                            <PlayerSvg fill={owner ? primary : on} />
+                            <Text style={[
+                                styles.player,
+                                { color: owner ? primary : on }
+                            ]}>
+                                {owner ? 'Anfitrião' : 'Visitante'}
+                            </Text>
+                        </View>
                     </View>
                 </View>
             </View>
-
         </RectButton>
     )
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { View, FlatList, Text } from 'react-native';
+import { View, FlatList } from 'react-native';
 import { Appointment } from '../../components/Appointment';
 import { ButtonAdd } from '../../components/ButtonAdd';
 import { CategorySelect } from '../../components/CategorySelect';
+import { ListDivider } from '../../components/ListDivider';
 import { ListHeader } from '../../components/ListHeader';
 import { Profile } from '../../components/Profile';
 import { styles } from './styles';
@@ -13,6 +14,18 @@ export function Home() {
     const appointments = [
         {
             id: '1',
+            guild: {
+                id: '1',
+                name: 'Lendário',
+                icon: null,
+                owner: false,
+            },
+            category: '1',
+            date: '22/06 às 20:40h',
+            description: ' É hoje que vamos chegar ao challenger sem perder uma partida da md10 🔥'
+        },
+        {
+            id: '2',
             guild: {
                 id: '1',
                 name: 'Lendário',
@@ -35,25 +48,25 @@ export function Home() {
                 <Profile />
                 <ButtonAdd />
             </View>
-
-            <View>
-                <CategorySelect
-                    categorySelected={category}
-                    setCategory={handlerCategorySelect}
+            <CategorySelect
+                categorySelected={category}
+                setCategory={handlerCategorySelect}
+            />
+            <View style={styles.content}>
+                <ListHeader
+                    title='Partidas agendadas'
+                    subtitle='Total 6'
                 />
-                <View style={styles.content}>
-                    <ListHeader
-                        title='Partidas agendadas'
-                        subtitle='Total 6'
-                    />
-                    <FlatList
-                        data={appointments}
-                        keyExtractor={item => item.id}
-                        renderItem={({ item }) => (
-                            <Appointment data={item} />
-                        )}
-                    />
-                </View>
+                <FlatList
+                    data={appointments}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => (
+                        <Appointment data={item} />
+                    )}
+                    ItemSeparatorComponent={() => <ListDivider />}
+                    style={styles.matches}
+                    showsVerticalScrollIndicator={false}
+                />
             </View>
         </View>
     )
