72efbbd981e9c652580a2ed4ef3e1e2e12cc2d5e
CELS0 <celsojr.300@gmail.com>
2021-11-09 22:26:38 -0300

    feat

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -4,31 +4,39 @@ import { SvgProps } from 'react-native-svg';
 import { LinearGradient } from 'expo-linear-gradient';
 import { theme } from '../../global/styles/theme';
 import { styles } from './styles';
-import { View,Text } from 'react-native';
+import { View, Text } from 'react-native';
 
 type Props = RectButtonProps & {
     title: string,
     icon: React.FC<SvgProps>,
     checked?: boolean,
+    hasCheckBox?: boolean,
 }
 
 export function Category({
     title,
     icon: Icon,
     checked = true,
+    hasCheckBox = true,
     ...rest
 }: Props) {
-    const { secondary50, secondary70 } = theme.colors
+    const { secondary40, secondary50, secondary70, secondary85 } = theme.colors
     return (
         <RectButton {...rest}>
             <LinearGradient
                 style={styles.container}
                 colors={[secondary50, secondary70]}
             >
-                <View style={[styles.content, { opacity: checked ? 1 : 0.4 }]}>
-                    <View 
-                    style={checked ? styles.checked : styles.check}
+                <LinearGradient
+                    style={[styles.content, { opacity: checked ? 1 : 0.5 }]}
+                    colors={[checked ? secondary85 : secondary50, secondary40]}
+                >
+                    {
+                        hasCheckBox &&
+                        <View
+                        style={checked ? styles.checked : styles.check}
                     />
+                    }
                     <Icon
                         width={48}
                         height={48}
@@ -36,7 +44,7 @@ export function Category({
                     <Text style={styles.title}>
                         {title}
                     </Text>
-                </View>
+                </LinearGradient>
             </LinearGradient>
         </RectButton>
     )
