042df620bb6990859d947482cadd1df636d5b4f3
CELS0 <celsojr.300@gmail.com>
2021-12-14 22:18:35 -0300

    feat

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { useAuth } from '../../hooks/auth';
 import { Avatar } from '../Avatar';
 import { ButtonAdd } from '../ButtonAdd';
 import { styles } from './styles';
 
 export function Profile() {
+    const { user } = useAuth();
     return (
         <View style={styles.container} >
 
-            <Avatar urlImage='https://github.com/CELS0.png' />
+            <Avatar urlImage={user.avatar} />
             <View>
                 <View style={styles.user}>
-                    
+
                     <Text style={styles.greeting}>
                         Olá,
                     </Text>
                     <Text style={styles.username}>
-                        Celso Jr
+                        {user.firstName}
                     </Text>
                 </View>
-                
+
                 <Text style={styles.message}>
                     Vamos jogar hoje!!
                 </Text>
