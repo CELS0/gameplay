4dd0eb874d8ad1d610a4f7d322ea83cb4302191d
CELS0 <celsojr.300@gmail.com>
2021-12-14 21:41:00 -0300

    fix

--- src/configs/discordAuth.ts
+++ src/configs/discordAuth.ts
@@ -1,7 +1,7 @@
-const REDIRECT_URI = 'https%3A%2F%2Fauth.expo.io%2Fgameplay';
+const REDIRECT_URI = 'https://auth.expo.io/@cels0/gameplay';
 const SCOPE = 'identify%20email%20connections%20guilds';
 const RESPONSE_TYPE = 'token';
-const CLIENT_ID = '919541078175141940';
+const CLIENT_ID = '920469523046490142';
 const CDN_IMAGE = 'https://cdn.discordapp.com';
 
 
@@ -12,3 +12,4 @@ export {
     CLIENT_ID,
     CDN_IMAGE,
 }
+
